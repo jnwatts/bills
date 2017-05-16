731bdf1e39aa3d6333db99f21df0df3b6af8f82b
Josh Watts <josh+github@sroz.net>
2017-05-16 10:57:14 -0400

    Add schema

---
+++ mysql_schema.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 16, 2017 at 10:54 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bills`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills_item_instances`
+--
+
+CREATE TABLE `bills_item_instances` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `due_date` date NOT NULL,
+  `type_id` int(10) UNSIGNED DEFAULT '0',
+  `paid_date` date DEFAULT NULL,
+  `automatic` tinyint(1) NOT NULL DEFAULT '0',
+  `notes` text,
+  `amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Instances of items';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills_item_types`
+--
+
+CREATE TABLE `bills_item_types` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(32) COLLATE utf8_bin NOT NULL,
+  `default_value` decimal(10,2) DEFAULT NULL,
+  `repeat_type` int(11) NOT NULL DEFAULT '0',
+  `description` text COLLATE utf8_bin,
+  `notes` text COLLATE utf8_bin,
+  `url` varchar(512) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='List of recurring and one-time expenses and incomes';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bills_item_instances`
+--
+ALTER TABLE `bills_item_instances`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `due_date` (`due_date`),
+  ADD KEY `item_type_id_due_date` (`type_id`) USING BTREE;
+
+--
+-- Indexes for table `bills_item_types`
+--
+ALTER TABLE `bills_item_types`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bills_item_instances`
+--
+ALTER TABLE `bills_item_instances`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1749;
+--
+-- AUTO_INCREMENT for table `bills_item_types`
+--
+ALTER TABLE `bills_item_types`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bills_item_instances`
+--
+ALTER TABLE `bills_item_instances`
+  ADD CONSTRAINT `bills_item_instances_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `bills_item_types` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
