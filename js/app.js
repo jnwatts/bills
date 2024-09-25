23e409bd9f19c4f26046e580fc54131f4a0a416e
Josh <josh_github@sroz.net>
2024-09-24 20:48:58 -0400

    app: Handle BALANCE entries

--- js/app.js
+++ js/app.js
@@ -81,6 +81,10 @@ var app = window.app = window.app || {};
         var item = self.items()[i];
         if (!item)
           return undefined;
+        if (item.type_id() == 2) {// BALANCE
+          item.balance(parseFloat(item.amount()).toFixed(2));
+          return item.balance();
+        }
         var balance_prev = parseFloat("0.00");
         if (i > 0) {
           var item_prev = self.items()[i-1];
